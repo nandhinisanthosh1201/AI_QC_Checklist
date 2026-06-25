bbf3fbcf81d7c0300af0c11d260c6a6cb94fc589
Lalitha Sri <laithaquade@gmail.com>
2026-06-25 10:48:05 +0530

    Romoved unwanted files

--- Mod 1-Roomn/fix.py
+++
@@ -1,5 +0,0 @@
-with open("Roomnn-1.py", "r", encoding="utf-8") as f:
-    c = f.read()
-c = c.replace('"view_id": vid,', '"view_id": int(vid),')
-with open("Roomnn-1.py", "w", encoding="utf-8") as f:
-    f.write(c)
