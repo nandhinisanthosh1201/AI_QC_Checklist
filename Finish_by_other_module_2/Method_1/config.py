d54b909b2db9ee9972621172f603f968b98f4ce6
Muthamil Selvi <muthamil@quadengg.com>
2026-07-27 10:51:30 +0530

    Replace old folder with new implementation

--- Finish_by_other_module_2/Method_1/config.py
+++ Finish_by_other_module_2/Method_1/config.py
@@ -5,4 +5,4 @@
 # ====================================================
 
 # Provide your OpenRouter API Key for Qwen
-OPENROUTER_API_KEY ="API_KEY"
+OPENROUTER_API_KEY ="Enter your OpenRouter API Key here"
